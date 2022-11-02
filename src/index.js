a7d87a0d2a26c6f0929f136a4a4d7badbb9bbaf5
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2022-11-02 22:20:30 +0330

    feat: change folder structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import "./reset.css"
+import './styles/index.css';
+import "./styles/reset.css"
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
