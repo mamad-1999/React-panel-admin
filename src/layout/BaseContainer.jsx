457279db163aad61d7e29eeca91331638e1bb6b8
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2022-11-06 20:05:45 +0330

    feat: add Header layout

--- src/layout/BaseContainer.jsx
+++ src/layout/BaseContainer.jsx
@@ -3,15 +3,16 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import { Stack } from "@mui/material";
+import Header from "./Header";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#6a6a6a" : "#fff",
+  backgroundColor: theme.palette.mode === "dark" ? "#343434" : "#fff",
   ...theme.typography.body2,
   textAlign: "center",
   color: theme.palette.text.secondary,
   borderRadius: 0,
   height: "100vh",
-  // maxHeight: "100vh"
 }));
 
 export default function BasicGrid() {
@@ -19,10 +20,14 @@ export default function BasicGrid() {
     <Box sx={{ flexGrow: 1 }}>
       <Grid container>
         <Grid item xs={3}>
-          <Item sx={{ borderRight: "2px solid #fff" }}>xs=3</Item>
+          <Item sx={{ borderRight: "2px solid #fff" }}>Mohammad</Item>
         </Grid>
         <Grid item xs={9}>
-          <Item>xs=9</Item>
+          <Item>
+            <Stack>
+              <Header />
+            </Stack>
+          </Item>
         </Grid>
       </Grid>
     </Box>
