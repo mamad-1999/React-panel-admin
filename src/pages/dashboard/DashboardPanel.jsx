40c5ceeb0ac57f33222a50f42aee5bb0b64cf910
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2022-11-10 11:27:20 +0330

    feat: add AreaChartPanel to DashboardPanel

--- src/pages/dashboard/DashboardPanel.jsx
+++ src/pages/dashboard/DashboardPanel.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import PanelLayout from "../../components/PanelLayout/PanelLayout";
+import { Paper, Typography, Grid } from "@mui/material";
+
+// import chart
+import AreaChartPanel from "../../components/AreaChart/AreaChartPanel";
 
 const DashboardPanel = () => {
   return (
     <PanelLayout>
-      <div>DashboardPanel</div>
+      <Grid container>
+        <Typography variant="h6">Dashboard</Typography>
+        <Grid item xs={12}>
+          <Paper
+            sx={{
+              padding: "20px",
+              marginTop: 2,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <AreaChartPanel />
+          </Paper>
+        </Grid>
+        <Grid item xs={8}></Grid>
+        <Grid item xs={4}></Grid>
+      </Grid>
     </PanelLayout>
   );
 };
