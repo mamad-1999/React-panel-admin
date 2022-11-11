e343cf37fd5dc44f864c9d86ea3712a383906ac5
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2022-11-11 09:52:53 +0330

    feat: add EditorPanel in AddPost

--- src/pages/posts/AddPost/AddPostPage.jsx
+++ src/pages/posts/AddPost/AddPostPage.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import PanelLayout from "../../../components/PanelLayout/PanelLayout";
 import { Grid, Paper, Typography, Box, TextField } from "@mui/material";
 
+// import editor
+// import EditorPanel from "../../../components/Editor/EditorPanel";
+import JoditEditor from "jodit-react";
+
 const AddPostPage = () => {
+  const editor = useRef(null);
+  const [content, setContent] = useState("");
+
   return (
     <PanelLayout>
       <Typography variant="h6">Add Post</Typography>
       <Grid container spacing={1}>
-        <Grid item xs={4}></Grid>
-        <Grid item xs={8}>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={9}>
           <Paper
             sx={{
               padding: "10px",
@@ -23,9 +30,31 @@ const AddPostPage = () => {
               sx={{
                 width: "100%",
                 maxWidth: "100%",
+                marginBottom: 2,
               }}
             >
               <TextField fullWidth label="Post Title" id="Post Title" />
+            </Box>
+            <Box
+              sx={{
+                width: "100%",
+                maxWidth: "100%",
+              }}
+            >
+              <JoditEditor
+                ref={editor}
+                value={content}
+                config={{
+                  readonly: false,
+                  style: {
+                    height: "300px",
+                  },
+                  theme: "dark"
+                }}
+                tabIndex={1}
+                onBlur={(value) => setContent(value)}
+                onChange={(value) => {}}
+              />
             </Box>
           </Paper>
         </Grid>
