b76fd32c0564b6a4e6aac8e555058820c2f39f1e
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2022-11-11 08:46:14 +0330

    feat: start Add post and add Post title

--- src/pages/posts/AddPost/AddPostPage.jsx
+++ src/pages/posts/AddPost/AddPostPage.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import PanelLayout from "../../../components/PanelLayout/PanelLayout";
+import { Grid, Paper, Typography, Box, TextField } from "@mui/material";
 
 const AddPostPage = () => {
   return (
     <PanelLayout>
-      <div>AddPostPage</div>
+      <Typography variant="h6">Add Post</Typography>
+      <Grid container spacing={1}>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={8}>
+          <Paper
+            sx={{
+              padding: "10px",
+              marginTop: 2,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              sx={{
+                width: "100%",
+                maxWidth: "100%",
+              }}
+            >
+              <TextField fullWidth label="Post Title" id="Post Title" />
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
     </PanelLayout>
   );
 };
